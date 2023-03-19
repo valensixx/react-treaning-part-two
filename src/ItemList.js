b45aeb1dd25c0e97ebb1074dab01dead9e6262a9
valensixx <valensixx@gmail.com>
2023-03-19 14:38:18 +0200

    some progress in Itemlist component

--- src/ItemList.js
+++ src/ItemList.js
@@ -4,7 +4,22 @@ import React, {useState} from "react";
 //onClick on item the color of the item choud change! 
 
 export default function ItemList(){
+    const [items, setItems] = useState(['Item 1', 'Item 2', 'Item 3']);
+    const [isHighlighted, setIsHighlighted] = useState([false, false, false]);
+
+    function handleClick(index){
+        //console.log(index);
+        isHighlighted[index] =!isHighlighted[index];
+        setIsHighlighted([...isHighlighted]);
+    }
+
     return(
-        <div>Hello </div>
+        <div>
+            <ul>
+                {items.map((item, index) => 
+                <li key={index} onClick={() => {handleClick(index)}}>{item} - {index}</li>)}
+            </ul>
+            <div>{JSON.stringify(isHighlighted)}</div>
+        </div>
     );
 };
--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import ItemList from './ItemList';
+import './ItemList.css';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
