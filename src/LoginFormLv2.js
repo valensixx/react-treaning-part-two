036f301223d798bf678ba681ea22c59d9d0729ed
valensixx <valensixx@gmail.com>
2023-03-19 15:43:03 +0200

    login form lv2 structure - done

--- src/LoginFormLv2.js
+++ src/LoginFormLv2.js
@@ -9,8 +9,19 @@ the form hides itself and displays 'Welcome, User1!'
 
 export default function LoginForm(){
     return (
-        <div>
-            Hello from login!
+        <div style={{marginTop:'20px'}}>
+            <h1>Login Form Lv2</h1>
+            <div>
+                <label>Username</label>
+                <input type="text"/>
+            </div>
+            <div>
+                <label>Password</label>
+                <input type="password"/>
+            </div>
+            <div>
+                <button type="button">Login</button>
+            </div>
         </div>
     );
 };
