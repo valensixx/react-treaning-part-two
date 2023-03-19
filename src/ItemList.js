1820ee4d9c57d70f996d1f0667194714ab56c036
valensixx <valensixx@gmail.com>
2023-03-19 14:11:42 +0200

    item list

---
+++ src/ItemList.js
@@ -0,0 +1,10 @@
+import React, {useState} from "react";
+
+//create item list with ul and il.
+//onClick on item the color of the item choud change! 
+
+export default function ItemList(){
+    return(
+        <div>Hello </div>
+    );
+};
