f756489887a7e33b0280595ee6b90c2a406ea18f
valensixx <valensixx@gmail.com>
2023-03-19 13:48:52 +0200

    react-reaining-part-2

--- src/App.js
+++ src/App.js
@@ -1,25 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from "react"; 
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+//create component with text "hello, how are you?".
+//create button witj var isShown with value false.
+//OnClick isShown == ture and the text is shown.
 
-export default App;
+export default function Hello(){
+    return(
+        <div>
+            <p>Hello world</p>
+        </div>
+    );
+};
