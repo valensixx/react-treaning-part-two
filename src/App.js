059d0e3c65ce85e14383ba85331b688ef7097e85
valensixx <valensixx@gmail.com>
2023-03-19 14:01:54 +0200

    tesk done show hide hello text using {? </>: ' '}

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import React, {useState} from "react"; 
-
+import Hello from "./Hello";
 //create component with text "hello, how are you?".
 //create button witj var isShown with value false.
 //OnClick isShown == ture and the text is shown.
 
-export default function Hello(){
+export default function App(){
+    const[isShown, setIsShown] = useState(false);
     return(
         <div>
-            <p>Hello world</p>
+            <button onClick = {()=>{
+                setIsShown(!isShown);
+            }}>Click me!</button>
+            {isShown ? < Hello /> : ''} 
+            {isShown && < Hello />} 
         </div>
     );
 };
---
+++ src/Hello.js
@@ -0,0 +1,7 @@
+import React, {useState} from "react";
+
+export default function Hello(){
+    return (
+        <div>Hello, how are you?</div>
+    );
+};
