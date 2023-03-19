c574661c7225261c1e62361e8219fa45db45b6d6
valensixx <valensixx@gmail.com>
2023-03-19 14:49:50 +0200

    loginForm lv 2

---
+++ src/LoginFormLv2.js
@@ -0,0 +1,16 @@
+import React,{useState} from "react";
+
+/*
+Create login form with userName and password.
+Create function login with onClick - Button.
+If user enters userName = 'user1' and pass = 'user1pass'
+the form hides itself and displays 'Welcome, User1!'
+*/
+
+export default function LoginForm(){
+    return (
+        <div>
+            Hello from login!
+        </div>
+    );
+};
--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import './index.css';
 import App from './App';
 import ItemList from './ItemList';
 import './ItemList.css';
+import LoginFormLv2 from './LoginFormLv2';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
@@ -11,6 +12,7 @@ root.render(
   <React.StrictMode>
     <App />
     <ItemList/> 
+    <LoginFormLv2 />
   </React.StrictMode>
 );
 
