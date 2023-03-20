ab258965dd46953bcfd4070bcc022205bee9c250
valensixx <valensixx@gmail.com>
2023-03-20 12:14:52 +0200

    checkBoxCounter

---
+++ src/CheckBox.js
@@ -0,0 +1,16 @@
+import React, {useState} from "react";
+
+/*
+crearte array with a few strings.
+create list with text of the elements and checkboxes
+create a counter in the component 
+the counter shows all of the active checkboxes
+*/
+
+export default function CheckBox(){
+    return(
+        <div>
+
+        </div>
+    );
+};
