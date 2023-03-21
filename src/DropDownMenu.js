1ced6e6ceaae0f1a4232a196bc17848f2b5ac727
valensixx <valensixx@gmail.com>
2023-03-21 12:40:43 +0200

    connecting components and some do design

--- src/DropDownMenu.js
+++ src/DropDownMenu.js
@@ -1,16 +1,16 @@
-import React ,{useState} from "react";
-
+import React, { useState } from "react";
+import DropDownMenuProps from './DropDownMenuProps';
 //create drop down menu with style props text on top of the menu, list of elements and func onChange.
 
-export default function DropDownMenu(){
+export default function DropDownMenu() {
     const options = [
-        {label: 'Option 1', value: 1},
-        {label: 'Option 2', value: 2},
-        {label: 'Option 3', value: 3},
-        ];
-    return(
-    <>
-
-    </>
+        { label: 'Option 1', value: 1 },
+        { label: 'Option 2', value: 2 },
+        { label: 'Option 3', value: 3 },
+    ];
+    return (
+        <div>
+            <DropDownMenuProps />
+        </div>
     );
 };
--- src/DropDownMenuProps.js
+++ src/DropDownMenuProps.js
@@ -1,7 +1,16 @@
 import React, {useState} from "react";
+import './DropDownMenuProps.css';
 
-export default function DDMP(){
+//at line 5 i am using different way of doing DDMP(perops) == DDMP({label, oprions, onChange})
+export default function DDMP({label, oprions, onChange}){
     return(
-        <></>
+        <div className="dropdown">
+            <label className="dd-label">
+                {label}
+            </label>
+            <select className="dd-select" onChange={onChange}>
+                <option value ="" >Select</option>
+            </select>
+        </div>
     );
 };
