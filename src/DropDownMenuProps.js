24c6a2fbb0ec1179cf6f3ddbe61043837176b727
valensixx <valensixx@gmail.com>
2023-03-21 13:07:27 +0200

    move opions in another function

--- src/DropDownMenuProps.js
+++ src/DropDownMenuProps.js
@@ -3,6 +3,15 @@ import './DropDownMenuProps.css';
 
 //at line 5 i am using different way of doing DDMP(perops) == DDMP({label, oprions, onChange})
 export default function DDMP({label, options, onChange}){
+
+    function renderOption(option){
+        return(
+            <option value = {option.value}>
+                {option.label}
+            </option>
+        );
+    }
+
     return(
         <div className="dropdown">
             <label className="dd-label">
@@ -10,11 +19,7 @@ export default function DDMP({label, options, onChange}){
             </label>
             <select className="dd-select" onChange={onChange}>
                 <option value ="" >Select</option>
-                {options.map(option => (
-                <option value={option.value}>
-                    {option.label}
-                </option>
-                ))}
+                {options.map(renderOption)}   
             </select>
         </div>
     );
