8176fabe79264d1a6c3fb33e0670d702f57bd984
valensixx <valensixx@gmail.com>
2023-03-22 14:57:52 +0200

    task done

--- src/ImageListPlus.js
+++ src/ImageListPlus.js
@@ -12,17 +12,26 @@ export default function ILP(props){
         },
         img: {
             width: '100%',
+        },
+
+        tag:{
+            background: 'silver',
+            color: 'blueviolet',
+            borderRadius: '20px',
+            margin: 10,
+            padding: 5,
         }
     }
 
     function renderSingleImage(item){
         return(
-            <div>
+            <div style={style.innerContainer}>
                 <img 
                     src={item.imagePath}
                     style={style.imagePath}
                 />
                 <h3>{item.title}</h3>
+                {item.tags.map(i => <span style={style.tag}>{i}</span>)}
             </div>
         );
     }
